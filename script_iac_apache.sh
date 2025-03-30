ee2aabdef973cc8f9520fa66c741d3cffe280369
Luiz Carlos Dev <luizcarlosdev@gmail.com>
2025-03-30 23:05:53 +0000

    adds y to installs and fix wget install

--- script_iac_apache.sh
+++ script_iac_apache.sh
@@ -5,14 +5,14 @@ apt-get update -y
 apt-get upgrade -y
 
 echo "Installing apache"
-apt-get install apache2
+apt-get install apache2 -y
 
 echo "Installing unzip"
-apt-get install unzip
+apt-get install unzip -y
 
 echo "Downloading application from github to /tmp"
 cd /tmp
-apt-get wget -y
+apt-get install wget -y
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 
 echo "Extracting the application to apache default folder"
